fc0af2f08be4fb207010d7dd599eb1b2cc2a97cf
태성 <idhts2081@gmail.com>
2026-05-02 17:42:00 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 
 app.get('/', (req, res) => {
-  res.send('https://github.com/idhts2081/_NaverAPI');
+  res.send('https://github.com/idhts2081/naverme-api');
 });
 
 app.get('/shorten', async (req, res) => {
